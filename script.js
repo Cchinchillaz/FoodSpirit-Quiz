56768c5ff99b1f10807158310ce73e71508c07d5
Nattarin Chetpattananondh <nattarinchet@gmail.com>
2024-06-11 19:53:08 +0700

    fix: style

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function playQuiz(){
     if (storyIndex < 20) {
       image.src = 'images/' + getStoryURL(storyIndex); 
     } else {
-      image.src = '';
+      image.remove();
       background.className = 'dialog-background dialog-20'; 
       
     }
